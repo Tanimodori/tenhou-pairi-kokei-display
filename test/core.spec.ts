857d31669e3218bf1770642561eda16af8792a38
Tanimodori <unknowner2014@gmail.com>
2022-08-28 19:35:00 +0800

    test: testing mjsub

--- test/core.spec.ts
+++ test/core.spec.ts
@@ -1,12 +1,14 @@
 import { describe, it, expect } from 'vitest';
-import { mjaka, mjcomp, mjtiles } from 'src/legacy';
+import { mjaka, MJArray, mjcomp, mjsub, mjtiles } from 'src/legacy';
 
-describe('Test tiles manipulating functions', () => {
-  it('mjtiles: can split tiles', () => {
+describe('mjtiles', () => {
+  it('can split tiles', () => {
     expect(mjtiles('12m3s0p5z')).toEqual(['1m', '2m', '3s', '0p', '5z']);
   });
+});
 
-  it('mjcomp: can compare tiles', () => {
+describe('mjcomp', () => {
+  it('can compare tiles', () => {
     // same suite
     expect(mjcomp('5s', '6s')).toBeLessThan(0);
     expect(mjcomp('5s', '5s')).toEqual(0);
@@ -17,11 +19,43 @@ describe('Test tiles manipulating functions', () => {
     expect(mjcomp('9p', '1s')).toBeLessThan(0);
     expect(mjcomp('9s', '1z')).toBeLessThan(0);
   });
+});
 
-  it('mjaka: can convert akadoras', () => {
+describe('mjaka', () => {
+  it('can convert akadoras', () => {
     expect(mjaka('5m')).toBe('0m');
     expect(mjaka('0m')).toBe('5m');
     expect(mjaka('5z')).toBe('5z');
     expect(mjaka('6s')).toBe('6s');
+  });
+});
+
+describe('mjsub', () => {
+  const getHand = (tiles = '456s50p') => {
+    const hand = mjtiles(tiles) as MJArray;
+    hand.mjfail = false;
+    return hand;
+  };
+
+  it('should skip failed hand', () => {
+    const hand = getHand();
+    hand.mjfail = true;
+    // TODO: return undefined is not a good practice
+    expect(mjsub(hand)).toEqual(undefined);
+  });
+
+  it('can subtract tiles', () => {
+    const hand = getHand();
+    expect(mjsub(hand, '5s')).toEqual(getHand('46s50p'));
+  });
+
+  it('subtracts akadoras when necessary', () => {
+    expect(mjsub(getHand(), '0s')).toEqual(getHand('46s50p'));
+    expect(mjsub(getHand(), '5p')).toEqual(getHand('456s0p'));
+    expect(mjsub(getHand(), '5p', '5p')).toEqual(getHand('456s'));
+  });
+
+  it('should fail when no tile can be subtracted', () => {
+    expect(mjsub(getHand(), '5z')?.mjfail).toBe(true);
   });
 });
