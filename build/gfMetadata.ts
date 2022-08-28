4906e44398c16466ec0b2bce15a4d82668081a45
Tanimodori <unknowner2014@gmail.com>
2022-08-28 16:22:07 +0800

    build: resolve version etc from package.json

--- build/gfMetadata.ts
+++ build/gfMetadata.ts
@@ -3,6 +3,7 @@
 import fs from 'fs';
 import { resolve } from 'path';
 import { PluginOption } from 'vite';
+import pkg from '../package.json';
 
 const metadata = `// ==UserScript==
 // @name         天鳳牌理好形表示
@@ -11,19 +12,19 @@ const metadata = `// ==UserScript==
 // @name:zh-TW   天鳳牌理好形表示
 // @name:en      Tenhou-Pairi Kokei display
 // @namespace    http://tanimodori.com/
-// @version      0.0.7
+// @version      ${pkg.version}
 // @description  天鳳牌理で一向聴の好形率を表示する
 // @description:zh  在天凤牌理中显示好形率
 // @description:zh-CN  在天凤牌理中显示好形率
 // @description:zh-TW  在天鳳牌理中顯示好形率
 // @description:en  Display Kokei percentage of ii-shan-ten in Tenhou-Pairi
-// @author       Tanimodori
+// @author       ${pkg.author}
 // @match        http://tenhou.net/2/*
 // @match        https://tenhou.net/2/*
 // @include      http://tenhou.net/2/*
 // @include      https://tenhou.net/2/*
 // @grant        none
-// @license      MIT
+// @license      ${pkg.license}
 // ==/UserScript==
 `;
 
