5320681903a613c19a5e900013806de45e94a4f6
Tanimodori <unknowner2014@gmail.com>
2022-08-30 06:44:43 +0800

    refactor: move ui related code to ui.ts

--- src/legacy.ts
+++ src/legacy.ts
@@ -1,3 +1,5 @@
+import { inject_css } from './ui';
+
 /** String in the webpage to check if calculating normal forms */
 export const S_P_QUERY = '一般形(七対国士を含まない)の計算結果 / 標準形';
 /** String in the webpage to check if calculating standard forms */
@@ -276,51 +278,6 @@ export const mjtenpaikei = (mjarr: MJArray) => {
     }
   }
   return result;
-};
-
-/** Inject Css style to the page */
-export const inject_css = () => {
-  const styles = `
-    .D {
-      position: relative;
-    }
-    .D .popup {
-      visibility: hidden;
-      width: 300px;
-      background-color: #ddd;
-      color: #fff;
-      text-align: center;
-      border-radius: 6px;
-      padding: 8px 0;
-      position: absolute;
-      z-index: 1;
-      top: 125%;
-      left: 50%;
-      margin-left: -150px;
-    }
-    .D .popup::before {
-      content: "";
-      position: absolute;
-      top: calc(0% - 10px);
-      left: 50%;
-      margin-left: -5px;
-      border-width: 5px;
-      border-style: solid;
-      border-color: transparent transparent #ddd transparent;
-    }
-    .D .popup.show {
-      visibility: visible;
-    }
-    .D .popup table {
-        text-align: initial;
-        margin-left: auto;
-        margin-right: auto;
-    }
-`;
-  const styleSheet = document.createElement('style');
-  styleSheet.type = 'text/css';
-  styleSheet.innerText = styles;
-  document.head.appendChild(styleSheet);
 };
 
 /**
